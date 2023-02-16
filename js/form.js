39197e11d1e97ce5cf9d6ab10672f6a3ef946f76
Laurel Perkins <lmperkins20@gmail.com>
2023-02-15 19:02:39 -0800

    fixed syntax errors, created popups for win and lose messages. Note that many functions have not been called yet, so if we have issues testing, that may be why

--- js/form.js
+++ js/form.js
@@ -97,28 +97,28 @@ function renderQuestions() {
 	document.getElementById('getusername').appendChild(username);
 
 	let q1h1 = document.createElement('h1');
-	q1h1.textContent(randQ1);
+	q1h1.textContent = randQ1;
 	document.getElementById('questionone').appendChild(q1h1);
 
 	let answersDiv1 = document.createElement('ul');
 	document.getElementById('questionOne').appendChild(answersDiv1);
 
 	let q2h2 = document.createElement('h2');
-	q2h2.textContent(randQ2);
+	q2h2.textContent = randQ2;
 	document.getElementById('questiontwo').appendChild(q2h2);
 
 	let answersDiv2 = document.createElement('ul');
 	document.getElementById('questionTwo').appendChild(answersDiv2);
 
 	let q3h3 = document.createElement('h3');
-	q3h3.textContent(randQ3);
+	q3h3.textContent = randQ3;
 	document.getElementById('questionthree').appendChild(q3h3);
 
 	let answersDiv3 = document.createElement('ul');
 	document.getElementById('questionThree').appendChild(answersDiv3);
 
 	let q4h4 = document.createElement('h4');
-	q4h4.textContent(randQ4);
+	q4h4.textContent = randQ4;
 	document.getElementById('questionfour').appendChild(q4h4);
 
 	let answersDiv4 = document.createElement('ul');
@@ -128,397 +128,397 @@ function renderQuestions() {
 	if (randQ1 === q1) {
 		// q1, append coolA1, neutA1
 		let q1a1 = document.createElement('li');
-		q1a1.textContent(coolA1);
+		q1a1.textContent=coolA1;
 		answersDiv1.appendChild(q1a1);
 		let q1a2 = document.createElement('li');
-		q1a2.textContent(neutA1);
+		q1a2.textContent=neutA1;
 		answersDiv1.appendChild(q1a2);
 	}
 	else if (randQ2 === q1) {
 		let q1a1 = document.createElement('li');
-		q1a1.textContent(coolA1);
+		q1a1.textContent=coolA1;
 		answersDiv2.appendChild(q1a1);
 		let q1a2 = document.createElement('li');
-		q1a2.textContent(neutA1);
+		q1a2.textContent=neutA1;
 		answersDiv2.appendChild(q1a2);
 	}
 	else if (randQ3 === q1) {
 		let q1a1 = document.createElement('li');
-		q1a1.textContent(coolA1);
+		q1a1.textContent=coolA1;
 		answersDiv3.appendChild(q1a1);
 		let q1a2 = document.createElement('li');
-		q1a2.textContent(neutA1);
+		q1a2.textContent=neutA1;
 		answersDiv3.appendChild(q1a2);
 	}
 	else if (randQ4 === q1) {
 		let q1a1 = document.createElement('li');
-		q1a1.textContent(coolA1);
+		q1a1.textContent=coolA1;
 		answersDiv4.appendChild(q1a1);
 		let q1a2 = document.createElement('li');
-		q1a2.textContent(neutA1);
+		q1a2.textContent=neutA1;
 		answersDiv4.appendChild(q1a2);
 	}
 	else if (randQ1 === q2) {
 		// q2, append hapA1, neutA2
 		let q2a1 = document.createElement('li');
-		q2a1.textContent(hapA1);
+		q2a1.textContent=hapA1;
 		answersDiv1.appendChild(q2a1);
 		let q2a2 = document.createElement('li');
-		q2a2.textContent(neutA2);
+		q2a2.textContent=neutA2;
 		answersDiv1.appendChild(q2a2);
 	}
 	else if (randQ2 === q2) {
 		let q2a1 = document.createElement('li');
-		q2a1.textContent(hapA1);
+		q2a1.textContent=hapA1;
 		answersDiv2.appendChild(q2a1);
 		let q2a2 = document.createElement('li');
-		q2a2.textContent(neutA2);
+		q2a2.textContent=neutA2;
 		answersDiv2.appendChild(q2a2);
 	}
 	else if (randQ3 === q2) {
 		let q2a1 = document.createElement('li');
-		q2a1.textContent(hapA1);
+		q2a1.textContent=hapA1;
 		answersDiv3.appendChild(q2a1);
 		let q2a2 = document.createElement('li');
-		q2a2.textContent(neutA2);
+		q2a2.textContent=neutA2;
 		answersDiv3.appendChild(q2a2);
 	}
 	else if (randQ4 === q2) {
 		let q2a1 = document.createElement('li');
-		q2a1.textContent(hapA1);
+		q2a1.textContent=hapA1;
 		answersDiv4.appendChild(q1a1);
 		let q2a2 = document.createElement('li');
-		qa2.textContent(neutA2);
+		qa2.textContent=neutA2;
 		answersDiv4.appendChild(q2a2);
 	}
 	else if (randQ1 === q3) {
 		// q3, append coolA2, madA1
 		let q3a1 = document.createElement('li');
-		q3a1.textContent(coolA2);
+		q3a1.textContent=coolA2;
 		answersDiv1.appendChild(q3a1);
 		let q3a2 = document.createElement('li');
-		q3a2.textContent(madA1);
+		q3a2.textContent=madA1;
 		answersDiv1.appendChild(q3a2);
 	}
 	else if (randQ2 === q3) {
 		let q3a1 = document.createElement('li');
-		q3a1.textContent(coolA2);
+		q3a1.textContent=coolA2;
 		answersDiv2.appendChild(q3a1);
 		let q3a2 = document.createElement('li');
-		q3a2.textContent(madA1);
+		q3a2.textContent=madA1;
 		answersDiv2.appendChild(q3a2);
 	}
 	else if (randQ3 === q3) {
 		let q3a1 = document.createElement('li');
-		q3a1.textContent(coolA2);
+		q3a1.textContent=coolA2;
 		answersDiv3.appendChild(q3a1);
 		let q3a2 = document.createElement('li');
-		q3a2.textContent(madA1);
+		q3a2.textContent=madA1;
 		answersDiv3.appendChild(q3a2);
 	}
 	else if (randQ4 === q3) {
 		let q3a1 = document.createElement('li');
-		q3a1.textContent(coolA2);
+		q3a1.textContent=coolA2;
 		answersDiv4.appendChild(q3a1);
 		let q3a2 = document.createElement('li');
-		q3a2.textContent(madA1);
+		q3a2.textContent=madA1;
 		answersDiv4.appendChild(q3a2);
 	}
 	else if (randQ1 === q4) {
 		// q4, append hapA2, madA2
 		let q4a1 = document.createElement('li');
-		q4a1.textContent(hapA2);
+		q4a1.textContent=hapA2;
 		answersDiv1.appendChild(q4a1);
 		let q4a2 = document.createElement('li');
-		q4a2.textContent(madA2);
+		q4a2.textContent=madA2;
 		answersDiv1.appendChild(q4a2);
 	}
 	else if (randQ2 === q4) {
 		let q4a1 = document.createElement('li');
-		q4a1.textContent(hapA2);
+		q4a1.textContent=hapA2;
 		answersDiv2.appendChild(q4a1);
 		let q4a2 = document.createElement('li');
-		q4a2.textContent(madA2);
+		q4a2.textContent=madA2;
 		answersDiv2.appendChild(q4a2);
 	}
 	else if (randQ3 === q4) {
 		let q4a1 = document.createElement('li');
-		q4a1.textContent(hapA2);
+		q4a1.textContent=hapA2;
 		answersDiv3.appendChild(q4a1);
 		let q4a2 = document.createElement('li');
-		q4a2.textContent(madA2);
+		q4a2.textContent=madA2;
 		answersDiv3.appendChild(q4a2);
 	}
 	else if (randQ4 === q4) {
 		let q4a1 = document.createElement('li');
-		q4a1.textContent(hapA2);
+		q4a1.textContent=hapA2;
 		answersDiv4.appendChild(q4a1);
 		let q4a2 = document.createElement('li');
-		q4a2.textContent(madA2);
+		q4a2.textContent=madA2;
 		answersDiv4.appendChild(q4a2);
 	}
 	else if (randQ1 === q5) {
 		// q5, append coolA3, hapA3
 		let q5a1 = document.createElement('li');
-		q5a1.textContent(coolA3);
+		q5a1.textContent=coolA3;
 		answersDiv1.appendChild(q5a1);
 		let q5a2 = document.createElement('li');
-		q5a2.textContent(hapA3);
+		q5a2.textContent=hapA3;
 		answersDiv1.appendChild(q5a2);
 	}
 	else if (randQ2 === q5) {
 		let q5a1 = document.createElement('li');
-		q5a1.textContent(coolA3);
+		q5a1.textContent=coolA3;
 		answersDiv2.appendChild(q5a1);
 		let q5a2 = document.createElement('li');
-		q5a2.textContent(hapA3);
+		q5a2.textContent=hapA3;
 		answersDiv2.appendChild(q5a2);
 	}
 	else if (randQ3 === q5) {
 		let q5a1 = document.createElement('li');
-		q5a1.textContent(coolA3);
+		q5a1.textContent=coolA3;
 		answersDiv3.appendChild(q5a1);
 		let q5a2 = document.createElement('li');
-		q5a2.textContent(hapA3);
+		q5a2.textContent=hapA3;
 		answersDiv3.appendChild(q5a2);
 	}
 	else if (randQ4 === q5) {
 		let q5a1 = document.createElement('li');
-		q5a1.textContent(coolA3);
+		q5a1.textContent=coolA3;
 		answersDiv4.appendChild(q5a1);
 		let q5a2 = document.createElement('li');
-		q5a2.textContent(hapA3);
+		q5a2.textContent=hapA3;
 		answersDiv4.appendChild(q5a2);
 	}
 	else if (randQ1 === q6) {
 		// q6, append neutA3, madA3
 		let q6a1 = document.createElement('li');
-		q6a1.textContent(neutA3);
+		q6a1.textContent=neutA3;
 		answersDiv1.appendChild(q6a1);
 		let q6a2 = document.createElement('li');
-		q6a2.textContent(madA3);
+		q6a2.textContent=madA3;
 		answersDiv1.appendChild(q6a2);
 	}
 	else if (randQ2 === q6) {
 		let q6a1 = document.createElement('li');
-		q6a1.textContent(neutA3);
+		q6a1.textContent=neutA3;
 		answersDiv2.appendChild(q6a1);
 		let q6a2 = document.createElement('li');
-		q6a2.textContent(madA3);
+		q6a2.textContent=madA3;
 		answersDiv2.appendChild(q6a2);
 	}
 	else if (randQ3 === q6) {
 		let q6a1 = document.createElement('li');
-		q6a1.textContent(neutA3);
+		q6a1.textContent=neutA3;
 		answersDiv3.appendChild(q6a1);
 		let q6a2 = document.createElement('li');
-		q6a2.textContent(madA3);
+		q6a2.textContent=madA3;
 		answersDiv3.appendChild(q6a2);
 	}
 	else if (randQ4 === q6) {
 		let q6a1 = document.createElement('li');
-		q6a1.textContent(neutA3);
+		q6a1.textContent=neutA3;
 		answersDiv4.appendChild(q6a1);
 		let q6a2 = document.createElement('li');
-		q6a2.textContent(madA3);
+		q6a2.textContent=madA3;
 		answersDiv4.appendChild(q6a2);
 	}
 	else if (randQ1 === q7) {
 		// q7, append hapA4, coolA4
 		let q7a1 = document.createElement('li');
-		q7a1.textContent(hapA4);
+		q7a1.textContent=hapA4;
 		answersDiv1.appendChild(q7a1);
 		let q7a2 = document.createElement('li');
-		q7a2.textContent(coolA4);
+		q7a2.textContent=coolA4;
 		answersDiv1.appendChild(q7a2);
 	}
 	else if (randQ2 === q7) {
 		let q7a1 = document.createElement('li');
-		q7a1.textContent(hapA4);
+		q7a1.textContent=hapA4;
 		answersDiv2.appendChild(q7a1);
 		let q7a2 = document.createElement('li');
-		q7a2.textContent(coolA4);
+		q7a2.textContent=coolA4;
 		answersDiv2.appendChild(q7a2);
 	}
 	else if (randQ3 === q7) {
 		let q7a1 = document.createElement('li');
-		q7a1.textContent(hapA4);
+		q7a1.textContent=hapA4;
 		answersDiv3.appendChild(q7a1);
 		let q7a2 = document.createElement('li');
-		q7a2.textContent(coolA4);
+		q7a2.textContent=coolA4;
 		answersDiv3.appendChild(q7a2);
 	}
 	else if (randQ4 === q7) {
 		let q7a1 = document.createElement('li');
-		q7a1.textContent(hapA4);
+		q7a1.textContent=hapA4;
 		answersDiv4.appendChild(q7a1);
 		let q7a2 = document.createElement('li');
-		q7a2.textContent(coolA4);
+		q7a2.textContent=coolA4;
 		answersDiv4.appendChild(q7a2);
 	}
 	else if (randQ1 === q8) {
 		// q8, append neutA4, madA4
 		let q8a1 = document.createElement('li');
-		q8a1.textContent(neutA4);
+		q8a1.textContent=neutA4;
 		answersDiv1.appendChild(q8a1);
 		let q8a2 = document.createElement('li');
-		q8a2.textContent(madA4);
+		q8a2.textContent=madA4;
 		answersDiv1.appendChild(q8a2);
 	}
 	else if (randQ2 === q8) {
 		let q8a1 = document.createElement('li');
-		q8a1.textContent(neutA4);
+		q8a1.textContent=neutA4;
 		answersDiv2.appendChild(q8a1);
 		let q8a2 = document.createElement('li');
-		q8a2.textContent(madA4);
+		q8a2.textContent=madA4;
 		answersDiv2.appendChild(q8a2);
 	}
 	else if (randQ3 === q8) {
 		let q8a1 = document.createElement('li');
-		q8a1.textContent(neutA4);
+		q8a1.textContent=neutA4;
 		answersDiv3.appendChild(q8a1);
 		let q8a2 = document.createElement('li');
-		q8a2.textContent(madA4);
+		q8a2.textContent=madA4;
 		answersDiv3.appendChild(q8a2);
 	}
 	else if (randQ4 === q8) {
 		let q8a1 = document.createElement('li');
-		q8a1.textContent(neutA4);
+		q8a1.textContent=neutA4;
 		answersDiv4.appendChild(q8a1);
 		let q8a2 = document.createElement('li');
-		q8a2.textContent(madA4);
+		q8a2.textContent=madA4;
 		answersDiv4.appendChild(q8a2);
 	}
 	else if (randQ1 === q9) {
 		// q9, append coolA5, madA5
 		let q9a1 = document.createElement('li');
-		q9a1.textContent(coolA5);
+		q9a1.textContent=coolA5;
 		answersDiv1.appendChild(q9a1);
 		let q9a2 = document.createElement('li');
-		q9a2.textContent(madA5);
+		q9a2.textContent=madA5;
 		answersDiv1.appendChild(q9a2);
 	}
 	else if (randQ2 === q9) {
 		let q9a1 = document.createElement('li');
-		q9a1.textContent(coolA5);
+		q9a1.textContent=coolA5;
 		answersDiv2.appendChild(q9a1);
 		let q9a2 = document.createElement('li');
-		q9a2.textContent(madA5);
+		q9a2.textContent=madA5;
 		answersDiv2.appendChild(q9a2);
 	}
 	else if (randQ3 === q9) {
 		let q9a1 = document.createElement('li');
-		q9a1.textContent(coolA5);
+		q9a1.textContent=coolA5;
 		answersDiv3.appendChild(q9a1);
 		let q9a2 = document.createElement('li');
-		q9a2.textContent(madA5);
+		q9a2.textContent=madA5;
 		answersDiv3.appendChild(q9a2);
 	}
 	else if (randQ4 === q9) {
 		let q9a1 = document.createElement('li');
-		q9a1.textContent(coolA5);
+		q9a1.textContent=coolA5;
 		answersDiv4.appendChild(q9a1);
 		let q9a2 = document.createElement('li');
-		q9a2.textContent(madA5);
+		q9a2.textContent=madA5;
 		answersDiv4.appendChild(q9a2);
 	}
 	else if (randQ1 === q10) {
 		// q10, append hapA5, neutA5
 		let q10a1 = document.createElement('li');
-		q10a1.textContent(hapA5);
+		q10a1.textContent=hapA5;
 		answersDiv1.appendChild(q10a1);
 		let q10a2 = document.createElement('li');
-		q10a2.textContent(neutA5);
+		q10a2.textContent=neutA5;
 		answersDiv1.appendChild(q10a2);
 	}
 	else if (randQ2 === q10) {
 		let q10a1 = document.createElement('li');
-		q10a1.textContent(hapA5);
+		q10a1.textContent=hapA5;
 		answersDiv2.appendChild(q10a1);
 		let q10a2 = document.createElement('li');
-		q10a2.textContent(neutA5);
+		q10a2.textContent=neutA5;
 		answersDiv2.appendChild(q10a2);
 	}
 	else if (randQ3 === q10) {
 		let q10a1 = document.createElement('li');
-		q10a1.textContent(hapA5);
+		q10a1.textContent=hapA5;
 		answersDiv3.appendChild(q10a1);
 		let q10a2 = document.createElement('li');
-		q10a2.textContent(neutA5);
+		q10a2.textContent=neutA5;
 		answersDiv3.appendChild(q10a2);
 	}
 	else if (randQ4 === q10) {
 		let q10a1 = document.createElement('li');
-		q10a1.textContent(hapA5);
+		q10a1.textContent=hapA5;
 		answersDiv4.appendChild(q10a1);
 		let q10a2 = document.createElement('li');
-		q10a2.textContent(neutA5);
+		q10a2.textContent=neutA5;
 		answersDiv4.appendChild(q10a2);
 	}
 	else if (randQ1 === q11) {
 		// q11, append coolA6, neutA6
 		let q11a1 = document.createElement('li');
-		q11a1.textContent(coolA6);
+		q11a1.textContent=coolA6;
 		answersDiv1.appendChild(q11a1);
 		let q11a2 = document.createElement('li');
-		q11a2.textContent(neutA6);
+		q11a2.textContent=neutA6;
 		answersDiv1.appendChild(q11a2);
 	}
 	else if (randQ2 === q11) {
 		let q11a1 = document.createElement('li');
-		q11a1.textContent(coolA6);
+		q11a1.textContent=coolA6;
 		answersDiv2.appendChild(q11a1);
 		let q11a2 = document.createElement('li');
-		q11a2.textContent(neutA6);
+		q11a2.textContent=neutA6;
 		answersDiv2.appendChild(q11a2);
 	}
 	else if (randQ3 === q11) {
 		let q11a1 = document.createElement('li');
-		q11a1.textContent(coolA6);
+		q11a1.textContent=coolA6;
 		answersDiv3.appendChild(q11a1);
 		let q11a2 = document.createElement('li');
-		q11a2.textContent(neutA6);
+		q11a2.textContent=neutA6;
 		answersDiv3.appendChild(q11a2);
 	}
 	else if (randQ4 === q11) {
 		let q11a1 = document.createElement('li');
-		q11a1.textContent(coolA6);
+		q11a1.textContent=coolA6;
 		answersDiv4.appendChild(q11a1);
 		let q11a2 = document.createElement('li');
-		q11a2.textContent(neutA6);
+		q11a2.textContent=neutA6;
 		answersDiv4.appendChild(q11a2);
 	}
 	else if (randQ1 === q12) {
 		// q12, append hapA6, madA6
 		let q12a1 = document.createElement('li');
-		q12a1.textContent(hapA6);
+		q12a1.textContent=hapA6;
 		answersDiv1.appendChild(q12a1);
 		let q12a2 = document.createElement('li');
-		q12a2.textContent(madA6);
+		q12a2.textContent=madA6;
 		answersDiv1.appendChild(q12a2);
 	}
 	else if (randQ2 === q12) {
 		let q12a1 = document.createElement('li');
-		q12a1.textContent(hapA6);
+		q12a1.textContent=hapA6;
 		answersDiv2.appendChild(q12a1);
 		let q12a2 = document.createElement('li');
-		q12a2.textContent(madA6);
+		q12a2.textContent=madA6;
 		answersDiv2.appendChild(q12a2);
 	}
 	else if (randQ3 === q12) {
 		let q12a1 = document.createElement('li');
-		q12a1.textContent(hapA6);
+		q12a1.textContent=hapA6;
 		answersDiv3.appendChild(q12a1);
 		let q12a2 = document.createElement('li');
-		q12a2.textContent(madA6);
+		q12a2.textContent=madA6;
 		answersDiv3.appendChild(q12a2);
 	}
 	else if (randQ4 === q12) {
 		let q12a1 = document.createElement('li');
-		q12a1.textContent(hapA6);
+		q12a1.textContent=hapA6;
 		answersDiv4.appendChild(q12a1);
 		let q12a2 = document.createElement('li');
-		q12a2.textContent(madA6);
+		q12a2.textContent=madA6;
 		answersDiv4.appendChild(q12a2);
 	}
   return playerUsername;
